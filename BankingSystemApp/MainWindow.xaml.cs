8799d720eaf68e763835b36effe3663e52b27db3
Casperu20 <nicubunea20@gmail.com>
2025-10-25 21:29:24 +0300

    verify bank uniqueness name and fee update logic (testing for the moment)

--- BankingSystemApp/MainWindow.xaml.cs
+++ BankingSystemApp/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BankingSystemApp.Enums;
+using BankingSystemApp.Models;
+using BankingSystemApp.Services;
 
 namespace BankingSystemApp;
 
@@ -16,8 +19,20 @@ namespace BankingSystemApp;
 /// </summary>
 public partial class MainWindow : Window
 {
-    public MainWindow()
-    {
+    public MainWindow() {
         InitializeComponent();
+        
+        var service = new BankService();
+        
+        var bank1 = new Bank("BCR", "BCRROBU", BankLocation.TM, BankCountry.RO);
+        var bank2 = new Bank("BCR", "BCRDEBU", BankLocation.B, BankCountry.DE);
+        var bank3 = new Bank("BCR", "BCRROCT", BankLocation.CT, BankCountry.RO);
+        
+        service.AddBank(bank1); // Should work 
+        service.AddBank(bank2); // Should work again
+        service.AddBank(bank3); // Should ERROR -> same name & country
+        
+        service.ApplyFee(bank1, 25.50m);
+        service.ApplyFee(bank2, 10.00m);
     }
 }
---
+++ BankingSystemApp/Services/BankService.cs
@@ -0,0 +1,40 @@
+﻿using BankingSystemApp.Models;
+using BankingSystemApp.Enums;
+namespace BankingSystemApp.Services;
+
+public class BankService {
+    public List<Bank> Banks { get; set; }
+
+    public BankService() {
+        this.Banks = new List<Bank>();
+    }
+
+    public bool AddBank(Bank NewBank) {
+        // name of the banks must be unique, a bank can have the same name only if there are in different countries.
+        bool exists = false;
+
+        foreach (var bank in Banks) {
+            bool sameName = (bank.Name == NewBank.Name);
+            bool sameCountry = (bank.Country == NewBank.Country);
+
+            if (sameName && sameCountry) {
+                exists = true;
+                break;
+            }
+        }
+        
+        if (exists) {
+            Console.WriteLine($"Bank {NewBank.Name} already exists in {NewBank.Country}!!!");
+            return false;
+        }
+        
+        Banks.Add(NewBank);
+        Console.WriteLine($"Bank: {NewBank.Name} added succesfully in {NewBank.Country}");
+        return true;
+    }
+
+    public void ApplyFee(Bank bank, decimal fee) {
+        bank.Balance = bank.Balance + fee;
+        Console.WriteLine($"Fee = {fee} added to {bank.Name}. New balance: {bank.Balance}");
+    }
+}
